297315d0814c8329c35dfabe346adac43a09454d
Cisel Atakan <ciselatkn@gmail.com>
2017-05-05 18:04:46 +0300

    css fixes and comments

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 var xhr = new XMLHttpRequest();
 var comments = 'There are not any comments yet!';
 
+// Converts data.json's date to human readable string
 function parseDate(date) {
   var d = new Date(parseInt(date.replace(/\/Date\(/g, '').replace(')/', ''), 10));
   return d.getDate() + '/' + (d.getMonth() + 1) + '/' + (1900 + d.getYear()) + ' ' + d.getHours() + ':' + d.getMinutes();
 }
 
+// Takes comments string and a new comment object,
+// Returns html of comments
 function createComment(comments, c) {
   comments += '<div class="comment" id="' + c.Id + '">';
   comments += '<span class="user-name">' + c.User.DisplayName + '</span>';
@@ -16,6 +19,7 @@ function createComment(comments, c) {
   return comments;
 }
 
+// Adds the comment entered by user to html
 function addComment(e) {
   e.preventDefault();
   var now = Date.now();
@@ -36,12 +40,21 @@ function addComment(e) {
   document.getElementById('comment').value = '';
 }
 
+// Read the data from data.json
+xhr.open('GET', './data/data.json', true);
+
 xhr.onreadystatechange = function() {
+
+  // If we successfully get the data
   if (this.readyState == 4 && this.status == 200) {
+
+    // Insert data to the relevant places in html
     var res = JSON.parse(xhr.responseText);
     document.getElementById('image').src = res.Image
     document.getElementById('title').innerHTML = res.Title;
     document.getElementById('content').innerHTML = res.Content;
+
+    // if there are comments, update the comments string
     if (res.Comments.length) {
       comments = '';
       for (var i = 0; i < res.Comments.length; i += 1) {
@@ -49,8 +62,8 @@ xhr.onreadystatechange = function() {
         comments = createComment(comments, c);
       }
     }
-    document.getElementById('comments').innerHTML = comments;
   }
+  document.getElementById('comments').innerHTML = comments;
 };
-xhr.open('GET', './data/data.json', true);
+
 xhr.send();
